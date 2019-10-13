d3f0b7009a3a13cdb1d52ac4a5c2ceaf17e5ddf5
theapache64 <theapache64@gmail.com>
2019-10-13 22:53:17 +0530

    ◀️ backup

--- src/languages/java/features/JavaLayoutResFeature.kt
+++ src/languages/java/features/JavaLayoutResFeature.kt
@@ -2,7 +2,6 @@ package languages.java.features
 
 import base.LanguageSupport
 import languages.kotlin.features.KotlinLayoutResFeature
-import org.w3c.dom.HTMLSpanElement
 
 class JavaLayoutResFeature(languageSupport: LanguageSupport) : KotlinLayoutResFeature(languageSupport) {
 
